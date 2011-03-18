c321b162a020d6f7aed6c8050af0f08be1c645dc
Freddie FUJIWARA <github@zeg.la>
2011-03-18 22:11:25 +0900

    geo location function added

--- crons.py
+++ crons.py
@@ -3,35 +3,130 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
+from django.utils import simplejson
+import urllib
 import gdata.spreadsheet.text_db
 
 class Page(db.Model):
   page_id = db.IntegerProperty()
   html = db.TextProperty()
 
-class MakeXML:
+class Map(db.Model):
+  map_id = db.StringProperty()
+  prefecture = db.StringProperty()
+  city = db.StringProperty()
+  map_url = db.StringProperty()
+  mobile_map_url = db.StringProperty()
+  max_lng = db.FloatProperty()
+  mid_lng = db.FloatProperty()
+  min_lng = db.FloatProperty()
+  max_lat = db.FloatProperty()
+  mid_lat = db.FloatProperty()
+  min_lat = db.FloatProperty()
 
-    def make(self):
+class CrawlMap:
+
+    def crawl(self):
+      for gmap in Map.all():
+        json = urllib.urlopen(u"http://pipes.yahoo.com/pipes/pipe.run?_id=b7b3030968c2dcaf1560c469a6d57340&_render=json&map_url=%s" % (urllib.quote(gmap.map_url))).read()
+        data = simplejson.loads(json)
+        lngs = []
+        lats = []
+        for record in data["value"]["items"]:
+          lnglat = record["author"].split(",")
+          lngs.append(lnglat[0])
+          lats.append(lnglat[1])
+        gmap.max_lng = float(max(lngs))
+        gmap.min_lng = float(min(lngs))
+        gmap.mid_lng = gmap.min_lng + (gmap.max_lng-gmap.min_lng)/2
+        gmap.max_lat = float(max(lats))
+        gmap.min_lat = float(min(lats))
+        gmap.mid_lat = gmap.min_lat + (gmap.max_lat-gmap.min_lat)/2
+        gmap.put()
+
+class CrawlSpread:
+
+    def crawl(self):
       client = gdata.spreadsheet.text_db.DatabaseClient('kyusuisho.appspot.com@gmail.com', 'ohsiusuyk')
       db = client.GetDatabases(name=u'kyusuisho.appspot.com')[0]
-      xml=MakeXML.make_xml(db.GetTables(name=u'root')[0])
+      table = db.GetTables(name=u'root')[0]
+      xml=CrawlSpread.make_xml(table)
+
       page = Page.gql('WHERE page_id = :1', 10).get()
       if page is None:
         page = Page(page_id=10,html=xml)
       else:
-        page.xml = xml
+        page.html = xml
       page.put()
+
+      html=CrawlSpread.crawl_spread(table)
+      page = Page.gql('WHERE page_id = :1', 1).get()
+      if page is None:
+        page = Page(page_id=1,html=html)
+      else:
+        page.html = html
+      page.put()
+
+      for record in table.GetRecords(1, 999999999):
+        gmap = Map.gql('WHERE map_id = :1',record.content[u"マップid英数字"] ).get()
+        if gmap is None:
+          gmap = Map(map_id        =record.content[u"マップid英数字"],
+                     prefecture    =record.content[u"都道府県"],
+                     city          =record.content[u"市区町村"],
+                     map_url       =record.content[u"マップurl"],
+                     mobile_map_url=record.content[u"携帯マップurl"],
+                    )
+        else:
+          gmap.prefecture    =record.content[u"都道府県"]
+          gmap.city          =record.content[u"市区町村"]
+          gmap.map_url       =record.content[u"マップurl"]
+          gmap.mobile_map_url=record.content[u"携帯マップurl"]
+        gmap.put()
+              
+
+    @classmethod
+    def html_escape(cls,text):
+      CrawlSpread.html_escape_table = {
+        #  u"&": u"&amp;",
+        u'"': u"&quot;",
+        u"'": u"&apos;",
+        u">": u"&gt;",
+        u"<": u"&lt;",
+      }
+      return u"".join(CrawlSpread.html_escape_table.get(c,c) for c in text)
 
     @classmethod
     def xml_escape(cls,text):
-      MakeXML.xml_escape_table = {
+      CrawlSpread.xml_escape_table = {
         u"&": u"&amp;",
         u'"': u"&quot;",
         u"'": u"&apos;",
         u">": u"&gt;",
         u"<": u"&lt;",
       }
-      return u"".join(MakeXML.xml_escape_table.get(c,c) for c in text)
+      return u"".join(CrawlSpread.xml_escape_table.get(c,c) for c in text)
+
+    @classmethod
+    def crawl_spread(cls,table):
+      html = u""
+      scripts = []
+      current_prefecture = u"";
+      for record in table.GetRecords(1, 999999999):
+        if current_prefecture != record.content[u"都道府県"]:
+          html += u"" if u"" == html else u"</ul></li>" 
+          current_prefecture =  record.content[u"都道府県"]
+          html += u"<li>"+CrawlSpread.html_escape(current_prefecture)+u"<ul>"
+        html+= u'<li>'
+        html+= u'<a href="'+CrawlSpread.html_escape(record.content[u"携帯マップurl"])+u'"'
+        html+= u' id="'+CrawlSpread.html_escape(record.content[u"マップid英数字"])+u'"'
+        html+= u' class="link"'
+        html+= u' name="'+CrawlSpread.html_escape(record.content[u"マップid英数字"])+u'">'
+        html+= CrawlSpread.html_escape(record.content[u"市区町村"])+u'</a></li>'
+        scripts.append(u'"'+CrawlSpread.html_escape(record.content[u"マップid英数字"])+u'":"'+CrawlSpread.html_escape(record.content[u"マップurl"])+u'"')
+      html += u'</ul></li><script type="text/javascript" id="gmaps">l={'
+      html += u",".join(scripts)
+      html += u'};</script>'
+      return html
 
     @classmethod
     def make_xml(cls,table):
@@ -42,81 +137,35 @@ class MakeXML:
         if current_prefecture != record.content[u"都道府県"]:
           xml += u"" if u"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<document>" == xml else u"</prefecture>" 
           current_prefecture =  record.content[u"都道府県"]
-          xml += u"<prefecture><name>"+MakeXML.xml_escape(current_prefecture)+u"</name>"
+          xml += u"<prefecture><name>"+CrawlSpread.xml_escape(current_prefecture)+u"</name>"
         xml+= u"<area>"
-        xml+= u"<id>"+MakeXML.xml_escape(record.content[u"マップid英数字"])+u"</id>"
-        xml+= u"<name>"+MakeXML.xml_escape(record.content[u"市区町村"])+u"</name>"
-        xml+= u"<link>"+MakeXML.xml_escape(record.content[u"マップurl"])+u"</link>"
-        xml+= u"<mobile_link>"+MakeXML.xml_escape(record.content[u"携帯マップurl"])+u"</mobile_link>"
+        xml+= u"<id>"+CrawlSpread.xml_escape(record.content[u"マップid英数字"])+u"</id>"
+        xml+= u"<name>"+CrawlSpread.xml_escape(record.content[u"市区町村"])+u"</name>"
+        xml+= u"<link>"+CrawlSpread.xml_escape(record.content[u"マップurl"])+u"</link>"
+        xml+= u"<mobile_link>"+CrawlSpread.xml_escape(record.content[u"携帯マップurl"])+u"</mobile_link>"
         xml+= u"</area>"
       xml += u"</prefecture></document>"
       return xml
 
-
-class MakeHTML:
-
-    def make(self):
-      client = gdata.spreadsheet.text_db.DatabaseClient('kyusuisho.appspot.com@gmail.com', 'ohsiusuyk')
-      db = client.GetDatabases(name=u'kyusuisho.appspot.com')[0]
-      html=MakeHTML.make_html(db.GetTables(name=u'root')[0])
-      page = Page.gql('WHERE page_id = :1', 1).get()
-      if page is None:
-        page = Page(page_id=1,html=html)
-      else:
-        page.html = html
-      page.put()
-
-    @classmethod
-    def html_escape(cls,text):
-      MakeHTML.html_escape_table = {
-        #  u"&": u"&amp;",
-        u'"': u"&quot;",
-        u"'": u"&apos;",
-        u">": u"&gt;",
-        u"<": u"&lt;",
-      }
-      return u"".join(MakeHTML.html_escape_table.get(c,c) for c in text)
-
-    @classmethod
-    def make_html(cls,table):
-      html = u""
-      scripts = []
-      current_prefecture = u"";
-      for record in table.GetRecords(1, 999999999):
-        if current_prefecture != record.content[u"都道府県"]:
-          html += u"" if u"" == html else u"</ul></li>" 
-          current_prefecture =  record.content[u"都道府県"]
-          html += u"<li>"+MakeHTML.html_escape(current_prefecture)+u"<ul>"
-        html+= u'<li>'
-        html+= u'<a href="'+MakeHTML.html_escape(record.content[u"携帯マップurl"])+u'"'
-        html+= u' id="'+MakeHTML.html_escape(record.content[u"マップid英数字"])+u'"'
-        html+= u' class="link"'
-        html+= u' name="'+MakeHTML.html_escape(record.content[u"マップid英数字"])+u'">'
-        html+= MakeHTML.html_escape(record.content[u"市区町村"])+u'</a></li>'
-        scripts.append(u'"'+MakeHTML.html_escape(record.content[u"マップid英数字"])+u'":"'+MakeHTML.html_escape(record.content[u"マップurl"])+u'"')
-      html += u'</ul></li><script type="text/javascript" id="gmaps">l={'
-      html += u",".join(scripts)
-      html += u'};</script>'
-      return html
-
-class MakeXMLHandler(webapp.RequestHandler):
+class CrawlMapHandler(webapp.RequestHandler):
     
     def __init__(self):
-        self.application = MakeXML()
+        self.application = CrawlMap()
     
     def get(self):
-        self.application.make()
+        json = self.application.crawl()
+        self.response.out.write(json)
 
-class MakeHTMLHandler(webapp.RequestHandler):
+class CrawlSpreadHandler(webapp.RequestHandler):
     
     def __init__(self):
-        self.application = MakeHTML()
+        self.application = CrawlSpread()
     
     def get(self):
-        self.application.make()
+        self.application.crawl()
 
 def main():
-    application = webapp.WSGIApplication([('/crons/make_html', MakeHTMLHandler),('/crons/make_xml', MakeXMLHandler)],
+    application = webapp.WSGIApplication([('/crons/crawl_spread', CrawlSpreadHandler),('/crons/crawl_map', CrawlMapHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
--- handlers.py
+++ handlers.py
@@ -8,6 +8,19 @@ class Page(db.Model):
   page_id = db.IntegerProperty()
   html = db.TextProperty()
 
+class Map(db.Model):
+  map_id = db.StringProperty()
+  prefecture = db.StringProperty()
+  city = db.StringProperty()
+  map_url = db.StringProperty()
+  mobile_map_url = db.StringProperty()
+  max_lng = db.FloatProperty()
+  mid_lng = db.FloatProperty()
+  min_lng = db.FloatProperty()
+  max_lat = db.FloatProperty()
+  mid_lat = db.FloatProperty()
+  min_lat = db.FloatProperty()
+
 class MainPage(webapp.RequestHandler):
   def get(self):
     page = Page.gql('WHERE page_id = :1', 1).get()
@@ -17,14 +30,56 @@ class MainPage(webapp.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'static/index.html')
     self.response.out.write(template.render(path, template_values))
 
-class APIPage(webapp.RequestHandler):
+class AreasAPI(webapp.RequestHandler):
   def get(self):
     page = Page.gql('WHERE page_id = :1', 10).get()
     self.response.headers['Content-Type'] = 'text/xml'
     self.response.out.write(page.html)
 
+class NearestAPI(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/xml'
+    lat = 0.0
+    lng = 0.0
+    try:
+      lat = float(self.request.get('lat'))
+      lng = float(self.request.get('lng'))
+    except:
+      lat = 0.0
+      lng = 0.0
+    distances_hash = {}
+    distances = []
+    for gmap in Map.all():
+      distance = pow(gmap.mid_lng-lng,2)+pow(gmap.mid_lat-lat,2)
+      distances_hash[str(distance)] = gmap.map_id
+      distances.append(distance)
+    gmap = Map.gql('WHERE map_id = :1',distances_hash[str(min(distances))] ).get()
+    xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<document>"
+    xml+= u"<area>"
+    xml+= u"<id>"+NearestAPI.xml_escape(gmap.map_id)+u"</id>"
+    xml+= u"<prefecture>"+NearestAPI.xml_escape(gmap.prefecture)+u"</prefecture>"
+    xml+= u"<city>"+NearestAPI.xml_escape(gmap.city)+u"</city>"
+    xml+= u"<map_url>"+NearestAPI.xml_escape(gmap.map_url)+u"</map_url>"
+    xml+= u"<mobile_map_url>"+NearestAPI.xml_escape(gmap.mobile_map_url)+u"</mobile_map_url>"
+    xml+= u"<mid_lat>"+str(gmap.mid_lat)+u"</mid_lat>"
+    xml+= u"<mid_lng>"+str(gmap.mid_lng)+u"</mid_lng>"
+    xml+= u"</area>"
+    xml+= u"</document>"
+    self.response.out.write(xml)
+
+  @classmethod
+  def xml_escape(cls,text):
+    NearestAPI.xml_escape_table = {
+      u"&": u"&amp;",
+      u'"': u"&quot;",
+      u"'": u"&apos;",
+      u">": u"&gt;",
+      u"<": u"&lt;",
+    }
+    return u"".join(NearestAPI.xml_escape_table.get(c,c) for c in text)
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage),('/api.xml', APIPage)],
+                                     [('/', MainPage),('/apis/areas.xml', AreasAPI),('/apis/nearest.xml', NearestAPI)],
                                      debug=True)
 
 def main():
